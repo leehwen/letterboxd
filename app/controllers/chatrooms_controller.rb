9cf8d5431f7927a2afe7eb767710e0312579151d
Melvin Lee <melvinleehw@gmail.com>
2024-04-26 15:54:45 +0800

    Chatroom show page completed

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,6 +4,7 @@ class ChatroomsController < ApplicationController
   end
 
   def show
+    @chatrooms = Chatroom.where(user1: current_user).or(Chatroom.where(user2: current_user))
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
